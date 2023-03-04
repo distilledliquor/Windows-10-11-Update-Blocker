8d35edbc8da568e2a666f65f63e80adf4b74b7c7
distilledliquor <54929046+distilledliquor@users.noreply.github.com>
2023-03-05 05:49:07 +0900

    Update Windows 11 Update Block.ps1

--- Windows 11 Update Block.ps1
+++ Windows 11 Update Block.ps1
@@ -11,3 +11,26 @@ New-ItemProperty -Force -Path $keyUX -PropertyType String -Name 'PauseQualityUpd
 New-ItemProperty -Force -Path $keyUX -PropertyType String -Name 'PauseUpdatesExpiryTime' -Value $dateEnd
 New-Item -Force -Path $keyAU
 New-ItemProperty -Force -Path $keyAU -PropertyType DWORD -Name 'NoAutoUpdate' -Value 1
+
+<# Completion message #>
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+[Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] > $null
+[string] $L1 = 'Windows 업데이트가 3000년 12월 31일에 다시 시작됩니다.'
+[string] $L2 = '　'
+[string] $L3 = 'Windows 업데이트 창의 업데이트 계속하기 버튼을 클릭하여 Windows 업데이트 기능을 복구하실 수 있습니다.'
+$toastDetail = @"
+<toast> 
+    <visual>
+        <binding template = "ToastImageAndText04">
+            <text id = "1">$($L1)</text>
+            <text id = "2">$($L2)</text>
+            <text id = "3">$($L3)</text>
+        </binding>
+    </visual> 
+</toast>
+"@
+$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+$xml.LoadXml($toastDetail)
+$toastNotification = [Windows.UI.Notifications.ToastNotification]::new($xml)
+[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier(' ').Show($toastNotification)
+
