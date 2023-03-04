30f3c3fed758f26626b5816c505b68e7ec838277
distilledliquor <54929046+distilledliquor@users.noreply.github.com>
2023-03-05 07:00:53 +0900

    Update Windows 11 Update Block.ps1

--- Windows 11 Update Block.ps1
+++ Windows 11 Update Block.ps1
@@ -3,14 +3,17 @@ $keyUX = 'Registry::HKLM\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings'
 $keyAU = 'Registry::HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU'
 $dateStart = '0000-01-01T00:00:00Z'
 $dateEnd = '3000-12-31T11:59:59Z'
+$getWinver = (Get-WmiObject -class Win32_OperatingSystem).Caption
 New-ItemProperty -Force -Path $keyUX -PropertyType String -Name 'PauseFeatureUpdatesStartTime' -Value $dateStart
 New-ItemProperty -Force -Path $keyUX -PropertyType String -Name 'PauseQualityUpdatesStartTime' -Value $dateStart
 New-ItemProperty -Force -Path $keyUX -PropertyType String -Name 'PauseUpdatesStartTime' -Value $dateStart
 New-ItemProperty -Force -Path $keyUX -PropertyType String -Name 'PauseFeatureUpdatesEndTime' -Value $dateEnd
 New-ItemProperty -Force -Path $keyUX -PropertyType String -Name 'PauseQualityUpdatesEndTime' -Value $dateEnd
 New-ItemProperty -Force -Path $keyUX -PropertyType String -Name 'PauseUpdatesExpiryTime' -Value $dateEnd
+if ($getWinver.Contains("Windows 11")) {
 New-Item -Force -Path $keyAU
 New-ItemProperty -Force -Path $keyAU -PropertyType DWORD -Name 'NoAutoUpdate' -Value 1
+}
 
 <# Completion message #>
 [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
@@ -33,4 +36,3 @@ $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
 $xml.LoadXml($toastDetail)
 $toastNotification = [Windows.UI.Notifications.ToastNotification]::new($xml)
 [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier(' ').Show($toastNotification)
-
